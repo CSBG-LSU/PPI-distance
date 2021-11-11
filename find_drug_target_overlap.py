8f05790aef6ff845cfdbf0f252a774cc45fd3169
Wentao Shi <wentao771@gmail.com>
2021-11-11 10:09:26 -0600

    rearranged

--- find_drug_target_overlap.py
+++ find_drug_target_overlap.py
@@ -6,34 +6,31 @@ Note: even though a single drug targets 2 different proteins, the protein neighb
 
 """
 # This script converts all the uniprot names to their respective grapg label numbers
-
-
 #!/usr/bin/python python3
-
 import pandas as pd
-import sys, argparse, os
 import networkx as nx
 from tqdm import tqdm
 
-from ensp_to_uniport_stitch import *
 
-# Step1 : convert ENSP-IDs to UniProt-IDs
-final_df = ensp_to_uniport()
+def ensp_to_uniport():
+	stitch = pd.read_csv("../total_az_mapped_to_stitch.data", sep="\t", usecols=["CIDs","ENSP-ID", "Scores"])
+	ensp_2_uniprot = pd.read_csv("../ensp_to_uniprot.tab", sep="\t")
+	sub_ensp_2_uniport = ensp_2_uniprot[["ENSP-ID", "Entry"]]
+	ensp_uniport = dict()
+	for entry in list(sub_ensp_2_uniport["ENSP-ID"].unique()):
+	    try:
+	        ensp_uniport[entry.split(",")[0]] = sub_ensp_2_uniport[sub_ensp_2_uniport["ENSP-ID"] == entry]["Entry"].values[0]
+	    except:
+	        print("Error!", entry)
 
-# Step2: Find all interacting proteins for a drug
+	final_df = pd.DataFrame(columns=["CIDs","ENSP-ID","Scores"])
+	for k, v in ensp_uniport.items():
+	    df = stitch[stitch["ENSP-ID"] == k]
+	    df = df.replace(df["ENSP-ID"].values, v)
+	    final_df = pd.concat([final_df, df], ignore_index=True)
 
-drug_prots = dict() # Format of the dictionary {CID:[Prot1, Prot2, .....], CID2:[Prot1, Port3, ....]}
-cids = final_df["CIDs"].unique()
-for cid in cids:
-	drug_prots[cid] = final_df[final_df["CIDs"] == cid]["ENSP-ID"].values
+	return final_df
 
-
-#Step3: Load the graph
-print("reading graph...")
-Graph = nx.read_gexf("../subgraph.gexf")
-
-#Step4 : Convert all the uniprots to label
-print("converting to lables....")
 def convert_uniprot_to_label(Graph, drug_prots):
 	nodes = dict(Graph.nodes(data=True))
 	nodes_1 = dict()
@@ -65,26 +62,42 @@ def convert_uniprot_to_label(Graph, drug_prots):
 	
 	return nodes, edges, drugs
 
-print("computing new weights....")
-nodes, edges, drugs = convert_uniprot_to_label(Graph,drug_prots)
-for source, target, attributes in Graph.edges(data=True):
-	attributes["Score"] = round((1.01-attributes["Score"]), 5)
+if __name__=="__main__":
+	# Step1 : convert ENSP-IDs to UniProt-IDs
+	final_df = ensp_to_uniport()
 
-print("running Dijkstra...")
-for i, _ in tqdm(enumerate(list(drugs.keys()))):
-	for j, _ in enumerate(list(drugs.keys())):
-		try:
-			if list(drugs.keys())[i] != list(drugs.keys())[j]:
-				for target1 in drugs[list(drugs.keys())[i]]:
-					for target2 in drugs[list(drugs.keys())[j]]:
-						target2_neighbors = Graph.neighbors(target2)
-						for n in target2_neighbors:
-							length, path = nx.single_source_dijkstra(Graph, target1, n, weight="Score")
-							print(length, path)
-			#elif list(drugs.keys())[i] == list(drugs.keys())[j]:
-			#	break
-			else:
+	# Step2: Find all interacting proteins for a drug
+	drug_prots = dict() # Format of the dictionary {CID:[Prot1, Prot2, .....], CID2:[Prot1, Port3, ....]}
+	cids = final_df["CIDs"].unique()
+	for cid in cids:
+		drug_prots[cid] = final_df[final_df["CIDs"] == cid]["ENSP-ID"].values
+
+	#Step3: Load the graph
+	print("reading graph...")
+	Graph = nx.read_gexf("../subgraph.gexf")
+
+	#Step4 : Convert all the uniprots to label
+	print("converting to lables....")
+	nodes, edges, drugs = convert_uniprot_to_label(Graph,drug_prots)
+
+	print("computing new weights....")
+	for source, target, attributes in Graph.edges(data=True):
+		attributes["Score"] = round((1.01-attributes["Score"]), 5)
+
+	print("running Dijkstra...")
+	for i, _ in tqdm(enumerate(list(drugs.keys()))):
+		for j, _ in enumerate(list(drugs.keys())):
+			try:
+				if list(drugs.keys())[i] != list(drugs.keys())[j]:
+					for target1 in drugs[list(drugs.keys())[i]]:
+						for target2 in drugs[list(drugs.keys())[j]]:
+							target2_neighbors = Graph.neighbors(target2)
+							for n in target2_neighbors:
+								length, path = nx.single_source_dijkstra(Graph, target1, n, weight="Score")
+								print(length, path)
+				#elif list(drugs.keys())[i] == list(drugs.keys())[j]:
+				#	break
+				else:
+					break
+			except:
 				break
-		except:
-			break
-
